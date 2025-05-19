ae41a5ad9fbf64449ff9cc9516b685e0416df6cd
pratik kad <pratikkad22@gmail.com>
2025-05-19 21:47:49 +0530

    book schema created

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ import cookieParser from 'cookie-parser'
 import { connectToDatabase } from './utils/db.js'
 import userRoutes from './routes/user.routes.js'
 import cloudinaryConnect from './utils/cloudinary.js'
+import fileUpload from 'express-fileupload'
+import bookRoutes from './routes/books.routes.js'
+
 
 dotenv.config()
 const app=express()
@@ -20,11 +23,15 @@ app.use(
       allowedHeaders: ["Content-Type", "Authorization"],
     })
 );
-
+app.use(fileUpload({
+  useTempFiles: true, // Allows storing files temporarily for processing
+  tempFileDir: '/tmp/' // Temporary directory for file uploads
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/books', bookRoutes);
 
 app.get('/', (req,res)=>{
     res.send("Hey there..........!")
--- server/models/books.model.js
+++ server/models/books.model.js
@@ -1,36 +1,44 @@
-const mongoose = require("mongoose");
-
+import mongoose from "mongoose";
 const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true, maxlength: 255 },
-  author: [
-    {
-      name: { type: String, required: true },
-      bio: { type: String },
-      photo: { type: String } // URL or path to the author's photo
-    }
-  ],
-  publisher: { type: String, maxlength: 100 },
-  published_year: { type: Number },
-  category: [String], // Array of category names
-  total_copies: { type: Number, required: true },
-  available_copies: { type: Number, required: true },
-  cover_image: { type: String, required: true }, // URL or path to the book's cover image
-  synopsis: { type: String }, // Brief summary of the book
-  tags: [String], // Array of tags or keywords
-  sample_pages: [
-    {
-      page_number: { type: Number, required: true },
-      image: { type: String, required: true } // URL or path to the demo page image
-    }
-  ],
-  uploaded_files: [
-    {
-      filename: { type: String }, // Name of the uploaded file
-      file_url: { type: String, required: true } // URL or path to the uploaded file
-    }
-  ],
+  title: {
+    type: String,
+    required: true,
+    maxlength: 255
+  },
+  author: [String],
+  publisher: {
+    type: String,
+    maxlength: 100
+  },
+  published_year: {
+    type: Number
+  },
+  category: [String], 
+  total_copies: {
+    type: Number,
+    required: true
+  },
+  available_copies: {
+    type: Number,
+    required: true
+  },
+  cover_image: {
+    type: String,
+    required: true
+  }, 
+  synopsis: {
+    type: String
+  }, 
+  tags: [String], 
+  sample_pages: {
+    type: [String],
+    default: [],
+  },
+  uploaded_files: {
+    type:String,
+  },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Book", bookSchema);
+export default mongoose.model("Book", bookSchema);
