524900e365f0968e6c61e09f956b80993209fef8
pratik kad <pratikkad22@gmail.com>
2025-05-19 23:15:35 +0530

    books controller updated

--- server/controller/books.controller.js
+++ server/controller/books.controller.js
@@ -1,4 +1,5 @@
 import booksModel from "../models/books.model.js";
+import Borrowing from "../models/borrow.model.js";
 import {
   uploadFileToCloudinary,
   uploadImageToCloudinary
@@ -210,6 +211,163 @@ export const updateBook = async (req, res) => {
       success: false,
       message: "Issue in updating book!",
       error: error.message,
+    });
+  }
+};
+
+export const borrowBook = async (req, res)=>{
+    const userId = req.user.id;
+    const bookId =req.params.bookId;
+    try {
+        const today = new Date();
+        const dueDate =  new Date();
+        dueDate.setDate(today.getDate() + 10); //10 days ahed
+
+        const borrowedBook = await Borrowing.create({
+            userId, 
+            bookId, 
+            dueDate
+        });
+
+        await booksModel.findByIdAndUpdate(
+        bookId,
+        { $inc: { available_copies: -1 } },                    
+        { new: true })
+
+        res.status(200).json({
+        success: true,
+        message: "Book borrowed successfully!",
+        borrowedBook
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+        success: false,
+        message: "Issue in borrowing book!",
+        error: error.message,
+        });
+    }
+};
+
+export const getBorrowedBooks= async (req,res)=>{
+    const userId= req.user.id;
+    try {
+
+        const books= await Borrowing.find({userId:userId});
+        if (!books) {
+            return res.status(400).json({
+            success: false,
+            message: "Books not available!",})
+        }
+        
+
+        res.status(200).json({
+        success: true,
+        message: "Books fetched successfully!",
+        books
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+        success: false,
+        message: "Issue in fetching borrowed book!",
+        error: error.message,
+        });
+    }
+}
+
+export const returnBook = async (req,res)=>{
+    const bookId=req.params.bookId;
+    try {
+        const book = await Borrowing.findOne({ book_id: bookId, status: "borrowed" });
+        if(!book){
+            return res.status(400).json({
+        success: false,
+        message: "Book is not available!",
+        });
+        }
+
+        book.status = "returned"
+        book.returnDate=new Date();
+        book.save();
+
+        await booksModel.findByIdAndUpdate(
+        bookId,
+        { $inc: { available_copies: 1 } },      
+        { new: true });
+
+        res.status(200).json({
+        success: true,
+        message: "Book returned successfully!",
+        book
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+        success: false,
+        message: "Issue in returning book!",
+        error: error.message,
+        });
+    }
+};
+
+
+export const borrowHistory= async (req,res)=>{
+    try {
+        const borrowedBooks = await Borrowing.find({});
+        if(!borrowedBooks){
+            return res.status(400).json({
+            success: false,
+            message: "Books not available!",
+        });
+        }
+
+        res.status(200).json({
+        success: true,
+        message: "Book fetched successfully!",
+        borrowedBooks
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+        success: false,
+        message: "Issue in fetching books!",
+        error: error.message,
+        });
+    }
+};
+
+export const searchBooks = async (req, res) => {
+  const search = req.body.search; 
+
+  try {
+    const searchCondition = search
+      ? {
+          $or: [
+            { title: { $regex: search, $options: "i" } },
+            { author: { $regex: search, $options: "i" } },
+            { publisher: { $regex: search, $options: "i" } },
+            { category: { $regex: search, $options: "i" } },
+            { tags: { $regex: search, $options: "i" } }
+          ]
+        }
+      : {}; 
+
+    const books = await booksModel.find(searchCondition);
+
+    res.status(200).json({
+      success: true,
+      message: "Books retrieved successfully!",
+      books
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error in searching books.",
+      error: error.message
     });
   }
 };
