5b63aa21ceea0e16b3e6bbce0b8652d6ae9e574e
pratik kad <pratikkad22@gmail.com>
2025-06-05 13:40:20 +0530

    roles added

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["student","staff","librarian","admin"],
+      enum: ["student","staff","admin"],
       default: "student"
     },
     isVerified: {
