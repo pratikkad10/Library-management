b149046a1a64f5b4a8084c196f6b9b5d879c9317
pratik kad <pratikkad22@gmail.com>
2025-06-05 13:34:58 +0530

    isStudent, iaAdmin and isStaff middleware added

--- server/middleware/user.middleware.js
+++ server/middleware/user.middleware.js
@@ -30,7 +30,64 @@ const isLoggedIn = async (req, res, next)=>{
             error:error.message
         })
     }
-    // next(); 
 }
 
-export {isLoggedIn}
+const isStudent = async (req, res, next)=>{
+    try {
+        const {user} = req;
+        if(user.role !== "student"){
+            return res.status(400).json({
+                success:false,
+                message:"You are not a student!"
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(400).json({
+            success:false,
+            message:"You are not a student!",
+            error:error.message
+        })
+    }
+}
+
+const isStaff = async (req, res, next)=>{
+    try {
+        const {user} = req;
+        if(user.role !== "staff"){
+            return res.status(400).json({
+                success:false,
+                message:"You are not a staff!"
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(400).json({
+            success:false,
+            message:"You are not a staff!",
+            error:error.message
+        })
+    }
+}
+
+const isAdmin = async (req, res, next)=>{
+    try {
+        const {user} = req;
+        if(user.role !== "admin"){
+            return res.status(400).json({
+                success:false,
+                message:"You are not an admin!"
+            })
+        }
+        next();
+    } catch (error) {
+        return res.status(400).json({
+            success:false,
+            message:"You are not an admin!",
+            error:error.message
+        })
+    }
+}
+
+
+export {isLoggedIn, isStudent, isStaff, isAdmin}
